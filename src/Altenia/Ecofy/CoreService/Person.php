b9cc316b7ccef653324f8c4a6bcf21502501934e
Young Suk Ahn <ys.ahnpark@gmail.com>
2014-09-16 17:27:04 -0400

    person more cols

--- src/Altenia/Ecofy/CoreService/Person.php
+++ src/Altenia/Ecofy/CoreService/Person.php
@@ -38,7 +38,7 @@ class Person extends Model {
 	 * @var array
 	 */
     protected $fillable = array('uuid','domain_sid','domain_id','created_by','created_dt','updated_by','updated_dt','update_counter','lang'
-        , 'id', 'user_sid', 'primary_lang', 'education_level', 'highlight', 'philosophy', 'goals', 'personality_type'
+        , 'id', 'user_sid', 'primary_lang', 'education_level', 'highlight', 'philosophy', 'goals', 'personality_type', 'name_lc', 'mobile_number'
         , 'location', 'country_cd', 'province_cd', 'district', 'address', 'postal_code'
         , 'additional_type', 'alternate_name', 'description', 'image', 'name', 'same_as', 'url', 'additional_name', 'address', 'affiliation', 'alumni_of', 'award', 'awards', 'birth_date', 'brand', 'children', 'colleague', 'colleagues', 'contact_point', 'contact_points', 'death_date', 'duns', 'email', 'family_name', 'fax_number', 'follows', 'gender', 'given_name', 'global_location_number', 'has_pos', 'home_location', 'honorific_prefix', 'honorific_suffix', 'interaction_count', 'isic_v4', 'job_title', 'knows', 'makes_offer', 'member_of', 'naics', 'nationality', 'owns', 'parent', 'parents', 'performer_in', 'related_to', 'seeks', 'sibling', 'siblings', 'spouse', 'tax_id', 'telephone', 'vat_id', 'work_location', 'works_for');
 
@@ -49,7 +49,7 @@ class Person extends Model {
      */
     private static $validation_rules_create = array(
         'id' => 'required|alpha_dash|min:4',
-		'name' => 'required|alpha_dash|min:4',
+		'name' => 'required|min:2',
 		'url' => 'url',
 		'country_cd' => 'min:2|max:3'
     	);
@@ -61,7 +61,7 @@ class Person extends Model {
      */
     private static $validation_rules_udpate = array(
         'id' => 'required|alpha_dash|min:4',
-		'name' => 'required|alpha_dash|min:4',
+		'name' => 'required|min:2',
 		'url' => 'url',
 		'country_cd' => 'min:2|max:3'
     	);
